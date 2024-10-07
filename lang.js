78c9ee2d7883361a91ee8cb992786c3e2c30b8a1
AGR-anchardo <anne.chardonnens@arch.be>
2024-10-07 09:44:24 +0200

    Update: main menu ("data") + numbers in 1st intro paragraph

--- lang.js
+++ lang.js
@@ -9,7 +9,7 @@ const translations = {
 		},
 		content: {
 			title: "The Resistance 1940 - 1945 database",
-			paragraph1: "<em>Resistance in Belgium</em> is the result of a pilot project run by CegeSoma/State Archives of Belgium. This platform enables research into thousands of people involved in <b>resistance activities during the Second World War in Belgium</b> or, in some cases, in neighbouring countries such as France in particular. It currently contains information on almost 400 resistance organisations and on more than <b>40,000 people</b> for whom a preparatory file was opened by the State Security Service (Sûreté de l’État/Staatsveiligheid) for the granting of the status of Intelligence and Intervention Agent (Agent de Renseignements et d’Action/Inlichtingen- en Actieagent). New data will be added regularly.",
+			paragraph1: "<em>Resistance in Belgium</em> is the result of a pilot project run by CegeSoma/State Archives of Belgium. This platform enables research into thousands of people involved in <b>resistance activities during the Second World War in Belgium</b> or, in some cases, in neighbouring countries such as France in particular. It currently contains information on more than <b>42,000 people</b>, representing <b>around a quarter of all resistance members</b>. These are individuals for whom a file related to Intelligence and Intervention Services was produced by the State Security Service (Sûreté de l’État/Staatsveiligheid). <a href=\"/wiki/Data/en\" style=\"color: #158f68;\">New data</a> will be added regularly.",
 			paragraph2: "<h5>Transversal search</h5>Would you like to carry out more advanced search queries, for example based on the municipality of residence or the membership in a resistance organisation? See our page on transversal search for more information.",
 			paragraph3: "<h5>FAQ & Re-use of data</h5> Questions about our platform? Do you need assistance for your research? Do you have any questions about the origin of the data? Please consult the <a href=\"/wiki/FAQ\" style=\"color: #158f68;\">FAQ</a> for further information.</br> Would you like to quote or re-use content found on <em>Resistance in Belgium</em>? Please consult our practical <a href=\"/wiki/licence\" style=\"color: #158f68;\">information page</a> to make sure to re-use contents in the correct manner.",
 			paragraph4: "<h5>The Resistance in Belgium </h5> Would you like to find out more about the Resistance in Belgium, its forms of action, structures, motivations and values? For more information, please visit our platform <a href=\"https://www.belgiumwwii.be/\" target=\"_blank\" style=\"color: #158f68;\">Belgium WWII</a> and the <a href=\"https://www.belgiumwwii.be/belgique-en-guerre/les-themes/la-resistance.html\" target=\"_blank\" style=\"color: #158f68;\">topical page about the Resistance</a> <em>(FR)</em>.",
@@ -31,11 +31,11 @@ const translations = {
 		infoText: "All data is not yet online and this will take place in several phases over several years. Stay tuned for more updates.",
 		about: "About",
 		faq: "FAQ",
-		data: "Data",
+		data: "Data access",
 		menuLinks: {
 			about: '/wiki/About/en',
 			faq: '/wiki/FAQ/en',
-			data: '#'
+			data: '/wiki/Data/en'
 		},
 		logos: {
 			arch: "State Archives of Belgium",
@@ -54,7 +54,7 @@ const translations = {
 		},
 		content: {
 			title: "La base de données Résistance 1940 - 1945",
-			paragraph1: "<em>Resistance in Belgium</em> est le fruit d’un projet pilote mené par le CegeSoma/Archives de l’État en Belgique. Cette plateforme vous permet d’effectuer des recherches sur les milliers de personnes impliquées dans des <b>activités de résistance durant la Seconde Guerre mondiale en Belgique</b> ou, dans certains cas, dans des pays voisins, en particulier la France. Actuellement, elle contient des informations sur plus de <b>40 000 personnes</b> pour lesquelles un dossier préparatoire au Statut d’Agent de Renseignements et d’Action a été produit par la Sûreté de l’État, ainsi que sur près de 400 organisations de résistance. De nouvelles données seront régulièrement ajoutées.",
+			paragraph1: "<em>Resistance in Belgium</em> est le fruit d’un projet pilote mené par le CegeSoma/Archives de l’État en Belgique. Cette plateforme vous permet d’effectuer des recherches sur les milliers de personnes impliquées dans des <b>activités de résistance durant la Seconde Guerre mondiale en Belgique</b> ou, dans certains cas, dans des pays voisins, en particulier la France. Actuellement, elle contient des informations sur plus de <b>42 000 personnes</b>, soit <b>environ un quart des résistant·e·s</b>. Il s'agit des personnes pour lesquelles un dossier en lien avec les services de Renseignements et d’Action a été produit par la Sûreté de l’État. De <a href=\"/wiki/Data/fr\" style=\"color: #158f68;\">nouvelles données</a> seront régulièrement ajoutées.",
 			paragraph2: "<h5>Recherches transversales</h5>Vous souhaitez effectuer des recherches plus larges, par exemple sur la base d’une commune de domicile ou de l’appartenance à des organisations de résistance ? Consultez notre page sur les recherches transversales pour plus d'informations.",
 			paragraph3: "<h5>FAQ & réutilisation des données</h5>Vous avez des questions sur notre plateforme ? Vous aimeriez de l'aide pour effectuer vos recherches ? Vous avez des questions sur l'origine des données ? Consultez notre <a href=\"/wiki/FAQ/fr\" style=\"color: #158f68;\">FAQ</a> pour plus d'informations. </br>Vous souhaitez citer ou réutiliser du contenu trouvé sur <em>Resistance in Belgium</em> ? Consultez notre <a href=\"/wiki/licence/fr\" style=\"color: #158f68;\">page d'information</a> pour vous assurer que vous réutilisez le contenu de façon adéquate.",
 			paragraph4: "<h5>La résistance en Belgique</h5>Vous souhaitez en savoir plus sur la Résistance en Belgique, ses formes d’actions, ses structures, ses motivations et ses valeurs ? Pour en savoir plus, consultez notre plateforme <a href=\"https://www.belgiumwwii.be/\" target=\"_blank\" style=\"color: #158f68;\">Belgium WWII</a> et la <a href=\"https://www.belgiumwwii.be/belgique-en-guerre/les-themes/la-resistance.html/\" target=\"_blank\" style=\"color: #158f68;\">page thématique consacrée à la Résistance</a>.",
@@ -76,11 +76,11 @@ const translations = {
 		infoText: "Toutes les données ne sont pas encore en ligne et cela se fera en plusieurs phases sur plusieurs années. Restez à l'écoute pour plus de mises à jour.",
 		about: "À propos",
 		faq: "FAQ",
-		data: "Données",
+		data: "Accès aux données",
 		menuLinks: {
 			about: '/wiki/About/fr',
 			faq: '/wiki/FAQ/fr',
-			data: '#'
+			data: '/wiki/Data/fr'
 		},
 		logos: {
 			arch: "Archives de l'État en Belgique",
@@ -99,7 +99,7 @@ const translations = {
 		},
 		content: {
 			title: "Verzetsdatabank 1940 - 1945",
-			paragraph1: "<em>Resistance in Belgium</em> is het resultaat van een pilootproject van het CegeSoma/Rijksarchief in België. Het platform maakt het mogelijk opzoekingen te doen over de duizenden mensen die <b>tijdens Wereldoorlog II betrokken waren bij verzetsdaden in België of</b>, in een aantal gevallen, in de buurlanden, voornamelijk in Frankrijk. Momenteel bevat de databank informatie over meer dan <b>40.000 personen</b> waarvoor de Staatsveiligheid een voorbereidend dossier had aangelegd voor het bekomen van het statuut van actie- en inlichtingsagent, en over bijna 400 verzetsorganisaties. Regelmatig worden nieuwe gegevens toegevoegd.",
+			paragraph1: "<em>Resistance in Belgium</em> is het resultaat van een pilootproject van het CegeSoma/Rijksarchief in België. Het platform maakt het mogelijk opzoekingen te doen over de duizenden mensen die <b>tijdens Wereldoorlog II betrokken waren bij verzetsdaden in België of</b>, in een aantal gevallen, in de buurlanden, voornamelijk in Frankrijk. Momenteel omvat ze gegevens over meer dan <b>42 000 personen</b>, dit is <b>ongeveer één vierde van de verzetslieden</b>. Het gaat om mensen over wie de Staatsveiligheid een dossier heeft opgesteld in verband met de Inlichtings- en Actiediensten. Regelmatig worden <a href=\"/wiki/Data/nl\" style=\"color: #158f68;\">nieuwe gegevens</a> toegevoegd.",
 			paragraph2: "<h5>Transversaal onderzoek </h5> Voor informatie over meer gedetailleerde opzoekingen, bijvoorbeeld op basis van een gemeente van verblijf of van de verbondenheid met een verzetsorganisatie, kan u terecht op de webpagina over transversaal onderzoek.",
 			paragraph3: "<h5>FAQ & hergebruik van de gegevens</h5>Heeft u nog vragen over ons platform, wil u hulp om opzoekingen te doen of heeft u vragen over de oorsprong van de gegevens, bekijk dan onze <a href=\"/wiki/FAQ/nl\" style=\"color: #158f68;\">FAQ</a> voor meer informatie. </br>Wil u inhoud die u heeft gevonden op <em>Resistance in Belgium</em> citeren of hergebruiken, raadpleeg dan onze <a href=\"/wiki/licence/nl\" style=\"color: #158f68;\">informatiepagina</a> om u ervan te vergewissen dat u de inhoud correct hergebruikt.",
 			paragraph4: "<h5>Het Verzet in België </h5> Voor meer informatie over het verzet in België, zijn acties, structuren, motivatie en waarden, verwijzen we naar ons platform <a href=\"https://www.belgiumwwii.be/\" target=\"_blank\" style=\"color: #158f68;\">Belgium WWII</a> en naar de <a href=\"https://www.belgiumwwii.be/belgique-en-guerre/les-themes/la-resistance.html/\" target=\"_blank\" style=\"color: #158f68;\">thematische webpagina over het Verzet</a>.",
@@ -121,11 +121,11 @@ const translations = {
 		infoText: "Nog niet alle gegevens zijn online beschikbaar en dit zal in meerdere fasen over meerdere jaren plaatsvinden. Blijf op de hoogte voor meer updates.",
 		about: "Over",
 		faq: "FAQ",
-		data: "Gegevens",
+		data: "Toegang gegevens",
 		menuLinks: {
 			about: '/wiki/About/nl',
 			faq: '/wiki/FAQ/nl',
-			data: '#'
+			data: '/wiki/Data/nl'
 		},
 		logos: {
 			arch: "Rijksarchief in België",
@@ -144,7 +144,7 @@ const translations = {
 		},
 		content: {
 			title: "Die Widerstandsdatenbank 1940 - 1945",
-			paragraph1: "<em>Resistance in Belgium</em> ist das Ergebnis eines vom CegeSoma/Belgisches Staatsarchiv durchgeführten Pilotprojekts. Die Plattform ermöglicht die Suche nach Tausenden von Personen, die <b>während des Zweiten Weltkriegs in Belgien in Widerstandsaktivitäten aktiv waren</b> oder in einigen Fällen auch in Nachbarländern, insbesondere in Frankreich. Derzeit enthält die Datenbank Informationen über mehr als <b>40.000 Personen</b>, zu denen die Staatssicherheit eine Vorbereitungsakte mit dem Status „Agent de Renseignements et d'Action“ (Interventions- und Geheimdienstagent) erstellt hat, sowie über fast 400 Widerstandsorganisationen. Neue Daten werden regelmäßig hinzugefügt.",
+			paragraph1: "<em>Resistance in Belgium</em> ist das Ergebnis eines vom CegeSoma/Belgisches Staatsarchiv durchgeführten Pilotprojekts. Die Plattform ermöglicht die Suche nach Tausenden von Personen, die <b>während des Zweiten Weltkriegs in Belgien in Widerstandsaktivitäten aktiv waren</b> oder in einigen Fällen auch in Nachbarländern, insbesondere in Frankreich. Derzeit enthält die Datenbank Informationen über mehr als <b>42.000 Personen</b>, was <b>etwa einem Viertel der Widerstandskämpfer</b> entspricht. Es handelt sich um Personen, für die von der Staatssicherheit eine Akte im Zusammenhang mit den Diensten für Nachrichten- und Aktionstätigkeiten erstellt wurde. <a href=\"/wiki/Data/en\" style=\"color: #158f68;\">Neue Daten</a> werden regelmäßig hinzugefügt.",
 			paragraph2: "<h5>Erweiterte Suche</h5>Möchten Sie umfassendere Nachforschungen durchführen, z. B. auf der Grundlage des Wohnorts einer Person oder Mitgliedschaft in Widerstandsorganisationen? Besuchen Sie unsere Seite zur erweiterten Suche für zusätzliche Informationen.",
 			paragraph3: "<h5>FAQ & Weiterverwendung von Daten</h5>Fragen zu Plattform? Hilfe bei Ihren Recherchen nötig? Fragen zur Herkunft der Daten? Ausführliche Informationen sind in den <a href=\"/wiki/FAQ/de\" style=\"color: #158f68;\">Häufig gestellten Fragen (FAQ)</a> zu finden. </br>Möchten Sie Inhalte, die Sie auf <em>Resistance in Belgium</em> gefunden haben, zitieren oder weiterverwenden? Bitte lesen Sie <a href=\"/wiki/licence/de\" style=\"color: #158f68;\">diese Informationen</a> und stellen Sie sicher, dass Sie die Inhalte ordnungsgemäß weiterverwenden.",
 			paragraph4: "<h5>Widerstand in Belgien </h5>Möchten Sie mehr über den Widerstand in Belgien, seine Aktionsformen, seine Strukturen, seine Motivationen und Werte erfahren? Weitere Informationen finden Sie auf unserer Plattform <a href=\"https://www.belgiumwwii.be/\" target=\"_blank\" style=\"color: #158f68;\">Belgium WWII</a> und die <a href=\"https://www.belgiumwwii.be/de/belgien-im-krieg/major-themes/la-resistance.html\" target=\"_blank\" style=\"color: #158f68;\">Themenseite zum Widerstand</a>."
@@ -167,11 +167,11 @@ const translations = {
 		infoText: "Noch sind nicht alle Daten online verfügbar, und dies wird in mehreren Phasen über mehrere Jahre hinweg geschehen. Bleiben Sie dran für weitere Updates.",
 		about: "Über",
 		faq: "FAQ",
-		data: "Daten",
+		data: "Datenzugang",
 		menuLinks: {
 			about: '/wiki/About/de',
 			faq: '/wiki/FAQ/de',
-			data: '#'
+			data: '/wiki/Data/de'
 		},
 		logos: {
 			arch: "Belgisches Staatsarchiv",
